d70f419a284db7f9f12a2913c3b791fa1a8fc32d
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-25 18:31:54 +0600

    project initialized

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-3xl text-center font-bold mt-12">
+        This is home page
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
