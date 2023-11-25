d5cccfed501579de4736b565e2305d207a360b1a
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-26 00:39:39 +0600

    installed firebase and created registration page

---
+++ src/pages/Registration/Registration.jsx
@@ -0,0 +1,9 @@
+const Registration = () => {
+  return (
+    <div>
+      <h2 className="text-3xl text-center font-bold">registration page</h2>
+    </div>
+  );
+};
+
+export default Registration;
