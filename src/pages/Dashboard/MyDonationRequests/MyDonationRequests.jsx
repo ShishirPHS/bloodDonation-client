58bd886708c17ecb0d25ac1948d58e1de10526e5
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-28 05:24:00 +0600

    condition added to myDonationRequests page if user's haven't donation request

--- src/pages/Dashboard/MyDonationRequests/MyDonationRequests.jsx
+++ src/pages/Dashboard/MyDonationRequests/MyDonationRequests.jsx
@@ -49,65 +49,74 @@ const MyDonationRequests = () => {
       <h4 className="font-montserrat font-bold text-center text-3xl mt-5 mb-12">
         All Donation Requests
       </h4>
-      <div>
-        <div className="overflow-x-auto bg-white p-4">
-          <table className="table table-xs">
-            <thead className="bg-[#EFE9E9]">
-              <tr>
-                <th className="py-3">#</th>
-                <th>Recipient Name</th>
-                <th>Recipient Location</th>
-                <th>Donation Date</th>
-                <th>Donation Time</th>
-                <th>Donation Status</th>
-                <th>Donor Information</th>
-                <th>Edit</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {ownDonationRequests?.map((request, idx) => (
-                <tr key={idx}>
-                  <td>{idx + 1}</td>
-                  <td>{request.recipientName}</td>
-                  <td>
-                    {request.recipientDistrict}, {request.recipientUpazila}
-                  </td>
-                  <td>{request.donationDate}</td>
-                  <td>{request.donationTime}</td>
-                  <td>{request.donationStatus}</td>
-                  <td></td>
-                  <td className="text-base">
-                    <button className="bg-[#EF3D32] text-white p-2 rounded-md hover:bg-[#4E4E4E]">
-                      <LuFileEdit></LuFileEdit>
-                    </button>
-                  </td>
-                  <td className="text-base">
-                    <button className="bg-[#EF3D32] text-white p-2 rounded-md hover:bg-[#4E4E4E]">
-                      <RiDeleteBin4Fill></RiDeleteBin4Fill>
-                    </button>
-                  </td>
-                </tr>
+      {ownDonationRequests.length === 0 && (
+        <p className="text-lg text-center font-poppins">
+          You do not created any donation request yet
+        </p>
+      )}
+      {ownDonationRequests.length > 0 && (
+        <>
+          <div>
+            <div className="overflow-x-auto bg-white p-4">
+              <table className="table table-xs">
+                <thead className="bg-[#EFE9E9]">
+                  <tr>
+                    <th className="py-3">#</th>
+                    <th>Recipient Name</th>
+                    <th>Recipient Location</th>
+                    <th>Donation Date</th>
+                    <th>Donation Time</th>
+                    <th>Donation Status</th>
+                    <th>Donor Information</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {ownDonationRequests?.map((request, idx) => (
+                    <tr key={idx}>
+                      <td>{idx + 1}</td>
+                      <td>{request.recipientName}</td>
+                      <td>
+                        {request.recipientDistrict}, {request.recipientUpazila}
+                      </td>
+                      <td>{request.donationDate}</td>
+                      <td>{request.donationTime}</td>
+                      <td>{request.donationStatus}</td>
+                      <td></td>
+                      <td className="text-base">
+                        <button className="bg-[#EF3D32] text-white p-2 rounded-md hover:bg-[#4E4E4E]">
+                          <LuFileEdit></LuFileEdit>
+                        </button>
+                      </td>
+                      <td className="text-base">
+                        <button className="bg-[#EF3D32] text-white p-2 rounded-md hover:bg-[#4E4E4E]">
+                          <RiDeleteBin4Fill></RiDeleteBin4Fill>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div>
+            <div className="pagination">
+              <button onClick={handlePreviousBtn}>Previous</button>
+              {pages?.map((page) => (
+                <button
+                  className={currentPage === page ? "selected" : ""}
+                  onClick={() => setCurrentPage(page)}
+                  key={page}
+                >
+                  {page + 1}
+                </button>
               ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div>
-        <div className="pagination">
-          <button onClick={handlePreviousBtn}>Previous</button>
-          {pages?.map((page) => (
-            <button
-              className={currentPage === page ? "selected" : ""}
-              onClick={() => setCurrentPage(page)}
-              key={page}
-            >
-              {page + 1}
-            </button>
-          ))}
-          <button onClick={handleNextBtn}>Next</button>
-        </div>
-      </div>
+              <button onClick={handleNextBtn}>Next</button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
