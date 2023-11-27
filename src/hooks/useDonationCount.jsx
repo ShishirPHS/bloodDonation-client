e871cf44b2ce2a1c58644e58135878afb0c3e326
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-28 03:39:50 +0600

    load donationCount for logged in user

--- src/hooks/useDonationCount.jsx
+++ src/hooks/useDonationCount.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import useAxiosPublic from "./useAxiosPublic";
+import useAuth from "./useAuth";
 
 const useDonationCount = () => {
+  const { user } = useAuth();
   const [donationsCount, setDonationsCount] = useState(null);
   const axiosPublic = useAxiosPublic();
 
-  axiosPublic.get("/donationsCount").then((res) => {
+  axiosPublic.get(`/donationsCount/${user?.email}`).then((res) => {
     setDonationsCount(res.data.count);
   });
 
--- src/pages/Dashboard/DonorHome/DonorHome.jsx
+++ src/pages/Dashboard/DonorHome/DonorHome.jsx
@@ -15,50 +15,53 @@ const DonorHome = () => {
       <h4 className="font-semibold text-xl font-montserrat text-center mt-10 mb-16">
         Hi, {user.displayName}. Welcome to our organization.
       </h4>
-      <div>
-        <div className="overflow-x-auto bg-white p-4">
-          <table className="table table-xs">
-            <thead className="bg-[#EFE9E9]">
-              <tr>
-                <th className="py-3">#</th>
-                <th>Recipient Name</th>
-                <th>Recipient Location</th>
-                <th>Donation Date</th>
-                <th>Donation Time</th>
-                <th>Donation Status</th>
-                <th>Donor Information</th>
-                <th>Edit</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {recentThree?.map((request, idx) => (
-                <tr key={idx}>
-                  <td>{idx + 1}</td>
-                  <td>{request.recipientName}</td>
-                  <td>
-                    {request.recipientDistrict}, {request.recipientUpazila}
-                  </td>
-                  <td>{request.donationDate}</td>
-                  <td>{request.donationTime}</td>
-                  <td>{request.donationStatus}</td>
-                  <td></td>
-                  <td className="text-base">
-                    <button className="bg-[#EF3D32] text-white p-2 rounded-md hover:bg-[#4E4E4E]">
-                      <LuFileEdit></LuFileEdit>
-                    </button>
-                  </td>
-                  <td className="text-base">
-                    <button className="bg-[#EF3D32] text-white p-2 rounded-md hover:bg-[#4E4E4E]">
-                      <RiDeleteBin4Fill></RiDeleteBin4Fill>
-                    </button>
-                  </td>
+
+      {ownDonationRequests.length > 0 && (
+        <div>
+          <div className="overflow-x-auto bg-white p-4">
+            <table className="table table-xs">
+              <thead className="bg-[#EFE9E9]">
+                <tr>
+                  <th className="py-3">#</th>
+                  <th>Recipient Name</th>
+                  <th>Recipient Location</th>
+                  <th>Donation Date</th>
+                  <th>Donation Time</th>
+                  <th>Donation Status</th>
+                  <th>Donor Information</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {recentThree?.map((request, idx) => (
+                  <tr key={idx}>
+                    <td>{idx + 1}</td>
+                    <td>{request.recipientName}</td>
+                    <td>
+                      {request.recipientDistrict}, {request.recipientUpazila}
+                    </td>
+                    <td>{request.donationDate}</td>
+                    <td>{request.donationTime}</td>
+                    <td>{request.donationStatus}</td>
+                    <td></td>
+                    <td className="text-base">
+                      <button className="bg-[#EF3D32] text-white p-2 rounded-md hover:bg-[#4E4E4E]">
+                        <LuFileEdit></LuFileEdit>
+                      </button>
+                    </td>
+                    <td className="text-base">
+                      <button className="bg-[#EF3D32] text-white p-2 rounded-md hover:bg-[#4E4E4E]">
+                        <RiDeleteBin4Fill></RiDeleteBin4Fill>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
