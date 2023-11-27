659f2116c5bad8beded262ea21b96556e72198f0
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-27 20:16:05 +0600

    added select option for select donation time

--- src/pages/Dashboard/CreateDonationRequests/CreateDonationRequests.jsx
+++ src/pages/Dashboard/CreateDonationRequests/CreateDonationRequests.jsx
@@ -11,6 +11,33 @@ const CreateDonationRequests = () => {
   const [district, upazila] = useAddress();
   const { user } = useAuth();
 
+  const time = [
+    "12:00 AM",
+    "01:00 AM",
+    "02:00 AM",
+    "03:00 AM",
+    "04:00 AM",
+    "05:00 AM",
+    "06:00 AM",
+    "07:00 AM",
+    "08:00 AM",
+    "09:00 AM",
+    "10:00 AM",
+    "11:00 AM",
+    "12:00 PM",
+    "01:00 PM",
+    "02:00 PM",
+    "03:00 PM",
+    "04:00 PM",
+    "05:00 PM",
+    "06:00 PM",
+    "07:00 PM",
+    "08:00 PM",
+    "09:00 PM",
+    "10:00 PM",
+    "11:00 PM",
+  ];
+
   const onSubmit = async (data) => {
     console.log(data);
   };
@@ -129,7 +156,7 @@ const CreateDonationRequests = () => {
               <p className="text-[#FF0000]">Full Address is required</p>
             )}
           </div>
-          <div className="grid grid-cols-2 gap-5">
+          <div className="grid grid-cols-2 gap-4">
             <div className="form-control mb-3 col-span-1">
               <label className="label font-semibold">
                 <span>Donation Date *</span>
@@ -147,12 +174,17 @@ const CreateDonationRequests = () => {
               <label className="label font-semibold">
                 <span>Donation Time *</span>
               </label>
-              <input
-                className="input input-bordered"
-                type="text"
-                placeholder="Donation Time"
+              <select
+                className="select select-bordered w-full"
                 {...register("donationTime", { required: true })}
-              />
+              >
+                <option value="">Select Donation Time</option>
+                {time.map((hour, idx) => (
+                  <option key={idx} value={hour}>
+                    {hour}
+                  </option>
+                ))}
+              </select>
               {errors.donationTime?.type === "required" && (
                 <p className="text-[#FF0000]">Donation Time is required</p>
               )}
