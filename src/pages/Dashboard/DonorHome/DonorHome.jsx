82a0f07c396af073dad04555a690ee3947f27f6b
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-28 08:22:39 +0600

    added view my all requests btn to donor home

--- src/pages/Dashboard/DonorHome/DonorHome.jsx
+++ src/pages/Dashboard/DonorHome/DonorHome.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 import useMyDonationRequests from "../../../hooks/useMyDonationRequests";
 import { LuFileEdit } from "react-icons/lu";
@@ -12,9 +13,10 @@ const DonorHome = () => {
 
   return (
     <div>
-      <h4 className="font-semibold text-xl font-montserrat text-center mt-10 mb-16">
+      <h4 className="font-semibold text-xl font-montserrat mt-10 mb-5">
         Hi, {user.displayName}. Welcome to our organization.
       </h4>
+      <h3 className="mb-10 text-3xl font-montserrat font-bold text-center">Your Recent Requests</h3>
 
       {ownDonationRequests.length > 0 && (
         <div>
@@ -60,6 +62,15 @@ const DonorHome = () => {
               </tbody>
             </table>
           </div>
+          {ownDonationRequests.length > 3 && (
+            <div className="flex justify-center items-center mt-10">
+              <Link to="/dashboard/my-donation-requests">
+                <button className="btn bg-[#EF3D32] px-9 py-4 text-white hover:bg-[#4E4E4E] transition-all duration-500 ease-in-out rounded-none">
+                  View My All Requests
+                </button>
+              </Link>
+            </div>
+          )}
         </div>
       )}
     </div>
