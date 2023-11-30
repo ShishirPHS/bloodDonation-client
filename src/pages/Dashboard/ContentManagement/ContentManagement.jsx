43cb21a78d69908e3ec3ae41d3efc620f1a01237
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-30 09:38:54 +0600

    added filter functionality to all blogs table

--- src/pages/Dashboard/ContentManagement/ContentManagement.jsx
+++ src/pages/Dashboard/ContentManagement/ContentManagement.jsx
@@ -2,10 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import BlogTableRow from "../../../components/BlogTableRow/BlogTableRow";
+import { FaFilter } from "react-icons/fa";
+import { useState } from "react";
+import "./ContentManagement.css";
 
 const ContentManagement = () => {
+  const [filter, setFilter] = useState("all");
+
   const axiosPublic = useAxiosPublic();
-  // load data for pagination table(all donation)
+  // load data for all blogs
   const { data: allBlogs = [] } = useQuery({
     queryKey: ["allBlogs"],
     queryFn: async () => {
@@ -13,6 +18,15 @@ const ContentManagement = () => {
       return result.data;
     },
   });
+
+  const handleFilterBtn = (status) => {
+    setFilter(status);
+  };
+
+  const filteredBlogs =
+    filter === "all"
+      ? allBlogs
+      : allBlogs.filter((blog) => blog.status === filter);
 
   return (
     <div>
@@ -30,6 +44,29 @@ const ContentManagement = () => {
           <h2 className="text-center font-bold font-montserrat text-3xl mt-5 mb-10">
             Content Management
           </h2>
+          <div className=" bg-white inline-flex  mb-2">
+            <details className="dropdown">
+              <summary className="btn filter-btn flex items-center hover:text-red-600">
+                <FaFilter></FaFilter>
+                <span className="ml-2 font-semibold">Filter by</span>
+              </summary>
+              <ul className="action-btn p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                <li>
+                  <button onClick={() => handleFilterBtn("all")}>All</button>
+                </li>
+                <li>
+                  <button onClick={() => handleFilterBtn("draft")}>
+                    Draft
+                  </button>
+                </li>
+                <li>
+                  <button onClick={() => handleFilterBtn("published")}>
+                    Published
+                  </button>
+                </li>
+              </ul>
+            </details>
+          </div>
           <div className="overflow-x-auto bg-white p-4">
             <table className="table table-xs">
               <thead className="bg-[#EFE9E9]">
@@ -43,7 +80,7 @@ const ContentManagement = () => {
                 </tr>
               </thead>
               <tbody>
-                {allBlogs?.map((blog, idx) => (
+                {filteredBlogs?.map((blog, idx) => (
                   <BlogTableRow key={idx} blog={blog} idx={idx}></BlogTableRow>
                 ))}
               </tbody>
