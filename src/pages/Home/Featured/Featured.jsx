9ee501cef33362b1fb41f13fe2c905687a6711b9
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-30 23:40:35 +0600

    featured section

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,17 @@
+const Featured = () => {
+  return (
+    <div className="bg-[url(https://i.ibb.co/yVSbJk6/2.jpg)] bg-cover my-28">
+      <div className="py-44">
+        <h2 className="font-montserrat font-semibold text-5xl text-center text-white">
+          Join with us and save life
+        </h2>
+        <p className="text-white text-center mt-6 text-xl max-w-[1000px] mx-auto font-poppins">
+          Together, we can make a difference in the lives of those in need. Join
+          our community of donors and be a hero in someones story.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
