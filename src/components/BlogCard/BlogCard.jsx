f0a7e21aa5f4786ba29dac532108cb2c560a5fa6
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-01-27 23:40:11 +0600

    fix blogCard design issue

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -8,34 +8,34 @@ const BlogCard = ({ blog }) => {
   const wordsPerParagraph = 25;
 
   if (words.length <= wordsPerParagraph) {
-    // Return the original paragraph if it's already wordsPerParagraph words or less
     return words;
   }
 
   const slicedParagraph = words.slice(0, wordsPerParagraph).join(" ");
 
   return (
-    <div className="border rounded-xl">
-      <img className="w-full" src={thumbnailImage} alt="" />
-      <div className="p-5">
-        <h3 className="text-3xl font-bold font-montserrat mb-7 text-[#111111]">
+    <div className="border rounded-xl flex flex-col">
+      <div className="flex-grow">
+        <img className="w-full h-[280px]" src={thumbnailImage} alt="" />
+        <h3 className="text-3xl p-5 pb-0 font-bold font-montserrat mb-7 text-[#111111]">
           {blogTitle}
         </h3>
-        <p
-          style={{ lineHeight: "30px" }}
-          className="font-medium text-[#666666] mb-6"
-        >
-          {slicedParagraph}...
-        </p>
-        <a
-          href="#"
-          className="font-bold text-xl flex items-center hover:text-[#FF0000]"
-        >
-          Read More
-          <span className="text-base ml-2">
-            <FaAnglesRight></FaAnglesRight>
-          </span>
-        </a>
+      </div>
+      <div className="p-5 pt-0">
+        <div>
+          <p
+            style={{ lineHeight: "30px" }}
+            className="font-medium text-[#666666] mb-6"
+          >
+            {slicedParagraph}...
+          </p>
+          <button className="font-bold text-xl flex justify-start items-center hover:text-[#FF0000]">
+            Read More
+            <span className="text-base ml-2">
+              <FaAnglesRight></FaAnglesRight>
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
