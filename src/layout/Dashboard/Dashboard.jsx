59ed1b91f0dd0e21b9341714183ff841cfcdf77f
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-03-05 23:35:04 +0600

    added a todo

--- src/layout/Dashboard/Dashboard.jsx
+++ src/layout/Dashboard/Dashboard.jsx
@@ -7,6 +7,7 @@ const Dashboard = () => {
   const [userData] = useUser();
 
   return (
+    // TODO: Have to create a responsive layout
     <div className="grid grid-cols-5 container mx-auto">
       <div className="min-h-screen col-span-1 bg-[#4E4E4E] p-5">
         <div className="dashboard">
@@ -14,6 +15,7 @@ const Dashboard = () => {
             <li>
               <NavLink to="/dashboard/profile">Profile</NavLink>
             </li>
+            {/* nav links for admin */}
             {userData?.role === "admin" &&
               userData?.role !== "donor" &&
               userData?.role !== "volunteer" && (
@@ -41,6 +43,7 @@ const Dashboard = () => {
                   </li>
                 </>
               )}
+            {/* nav links for donor */}
             {userData?.role === "donor" &&
               userData?.role !== "admin" &&
               userData?.role !== "volunteer" && (
@@ -60,6 +63,7 @@ const Dashboard = () => {
                   </li>
                 </>
               )}
+            {/* nav links for volunteer */}
             {userData?.role === "volunteer" &&
               userData?.role !== "donor" &&
               userData?.role !== "admin" && (
@@ -86,10 +90,9 @@ const Dashboard = () => {
                   </li>
                 </>
               )}
-
             <div className="h-[1px] w-full bg-white my-4"></div>
             <li>
-              <NavLink className='flex items-center justify-start' to="/">
+              <NavLink className="flex items-center justify-start" to="/">
                 <IoIosArrowBack className="mr-2"></IoIosArrowBack>
                 <span>Back to Home</span>
               </NavLink>
