58498fcd7f011cff02064aabeb98f0c417844484
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-03-16 17:39:50 +0600

    display donation request details to the donation request details page

--- src/pages/BloodRequestDetails/BloodRequestDetails.jsx
+++ src/pages/BloodRequestDetails/BloodRequestDetails.jsx
@@ -1,12 +1,87 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const BloodRequestDetails = () => {
   const { id } = useParams();
+  const axiosPublic = useAxiosPublic();
+
+  // load specific donation request data
+  const { data: donationRequestData = {}, isLoading } = useQuery({
+    queryKey: ["singleDonationRequest"],
+    queryFn: async () => {
+      const result = await axiosPublic.get(`/donation/${id}`);
+      return result.data;
+    },
+  });
+
+  const {
+    requesterName,
+    requesterEmail,
+    recipientName,
+    recipientDistrict,
+    recipientUpazila,
+    hospitalName,
+    fullAddress,
+    donationDate,
+    donationTime,
+    requestMessage,
+  } = donationRequestData;
 
   return (
     <div>
-      <div className="container mx-auto">
-        <h2>this is blood request details page. Details of {id}.</h2>
+      <div className="container mx-auto py-32">
+        <h2 className="text-center font-bold text-3xl mb-16">
+          Details of Blood Donation Request
+        </h2>
+        {isLoading ? (
+          <div className="text-center py-10">
+            <span className="loading loading-spinner loading-lg"></span>
+          </div>
+        ) : (
+          <div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Requester Name :</p>
+              <p className="w-1/2">{requesterName}</p>
+            </div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Requester Email :</p>
+              <p className="w-1/2">{requesterEmail}</p>
+            </div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Recipient Name :</p>
+              <p className="w-1/2">{recipientName}</p>
+            </div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Recipient District :</p>
+              <p className="w-1/2">{recipientDistrict}</p>
+            </div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Recipient Upazila :</p>
+              <p className="w-1/2">{recipientUpazila}</p>
+            </div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Hospital Name :</p>
+              <p className="w-1/2">{hospitalName}</p>
+            </div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Full Address :</p>
+              <p className="w-1/2">{fullAddress}</p>
+            </div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Donation Date :</p>
+              <p className="w-1/2">{donationDate}</p>
+            </div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Donation Time :</p>
+              <p className="w-1/2">{donationTime}</p>
+            </div>
+            <div className="flex gap-6 text-xl">
+              <p className="w-1/2 text-right font-bold">Request Message :</p>
+              <p className="w-1/2">{requestMessage}</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
