0599cba6d421d643b7295b5f205a602e1a3ae385
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-03-22 23:42:09 +0600

    added handleSubmit function to the CheckoutForm

--- src/pages/Funding/CheckoutForm.jsx
+++ src/pages/Funding/CheckoutForm.jsx
@@ -1,10 +1,44 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+  };
+
   return (
-    <form>
-      <CardElement />
-      <button>Submit</button>
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
     </form>
   );
 };
